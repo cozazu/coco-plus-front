2b29716b8bdb867c5400099f842799a78ac441b8
cozazu <aranzazuarcila@gmail.com>
2024-06-11 11:06:59 -0500

    position por role en validación

--- utils/formValidation/companyAddEmployeeValidation.ts
+++ utils/formValidation/companyAddEmployeeValidation.ts
@@ -8,7 +8,7 @@ export default function companyAddEmployeeValidation(values: IAddEmployeeInfo): 
   if (!/^\s*(\+\d{1,3}\s*)?\d{11,15}\s*$/.test(values.phone)) errors.phone = "Debe tener entre 11 y 15 caracteres";
   if (!/\S+@\S+\.\S+/.test(values.email)) errors.email = 'El correo electrónico es obligatorio';
   if (!/^[0-9-]{7,15}$/.test(values.identification)) errors.identification = 'La identificación debe tener entre 7 y 15 números';
-  if (!/^[a-zA-Z\s\-']{3,50}$/.test(values.role)) errors.position = 'El puesto debe tener entre 3 y 50 caracteres y puede incluir letras, espacios, guiones y apóstrofes';
+  if (!/^[a-zA-Z\s\-']{3,50}$/.test(values.position)) errors.position = 'El puesto debe tener entre 3 y 50 caracteres y puede incluir letras, espacios, guiones y apóstrofes';
 
   return errors;
 };
